0aecf8aac9257b851fdce00fc93e87d5f1e15b8f
Prashant TT <prashanttakkalaki1@gmail.com>
2025-07-29 12:59:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,2 +1,29 @@
-// main.tf
-// Terraform main configuration for AKS and related resources
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "ghaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "GitHubActions"
+  }
+}
