7126ca9814b4b28e833604e1afe044526cc52b51
Prashant TT <prashanttakkalaki1@gmail.com>
2025-07-29 13:09:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,4 @@
-variable "resource_group_name" {
-  default = "rg-github"
-}
-
-variable "location" {
-  default = "East US"
-}
-
-variable "aks_name" {
-  default = "aks-github"
-}
-
-variable "node_count" {
-  default = 1
-}
-
-variable "node_vm_size" {
-  default = "Standard_DS2_v2"
-}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
